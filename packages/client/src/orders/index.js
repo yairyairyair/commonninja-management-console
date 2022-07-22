9614ad2396fafc674bdfaf44bad4c7ef0eb72906
yairyairyair <24393372+yairyairyair@users.noreply.github.com>
2022-07-23 01:11:39 +0300

    improve orders page

--- packages/client/src/orders/index.js
+++ packages/client/src/orders/index.js
@@ -1,17 +1,85 @@
-import { Create, Datagrid, DateField, DateInput, Edit, EditButton, ReferenceManyField, required, SimpleForm, TextField, TextInput, ListGuesser } from "react-admin";
+import { Create, Datagrid, DateField, Edit, SimpleForm, TextField, List, ArrayField, NumberField } from "react-admin";
 import AddCardIcon from '@mui/icons-material/AddCard';
 
 const OrderIcon = AddCardIcon;
 
+// interface IOrder {
+//     id?: string;
+//     orderNumber?: string;
+//     totalPrice?: string;
+//     totalDiscount?: string;
+//     totalTax?: string;
+//     shippingTax?: string;
+//     note?: string;
+//     totalShipping?: string;
+//     status?: 'cancelled' | 'completed' | 'failed' | 'refunded' | 'pending' | 'processing' | 'on-hold' | 'partial';
+//     createdAt?: string;
+//     updatedAt?: string;
+//     billingAddress?: IAddress;
+//     shippingAddress?: IAddress;
+//     customer?: ICustomer;
+//     lineItems?: ILineItem[];
+// }
+
+// interface ICustomer {
+//     id?: string;
+//     createdAt?: string;
+//     updatedAt?: string;
+//     firstName?: string;
+//     lastName?: string;
+//     email?: string;
+//     phoneNumber?: string;
+//     acceptMarketing?: boolean;
+//     address?: IAddress;
+//     tags?: string[];
+//   }
+  
+//   interface IAddress {
+//     zip?: string;
+//     country?: string;
+//     countryCode?: string;
+//     state?: string;
+//     city?: string;
+//     address1?: string;
+//     address2?: string;
+//   }
+
+// verify order and types fields
+
 // TODO: copy this from console
-const OrderList = ListGuesser;
+const OrderList = () => (
+    <List>
+        <Datagrid bulkActionButtons={false}>
+            <TextField source="id" />
+            <TextField source="orderNumber" />
+            <TextField source="totalPrice" />
+            <TextField source="status" />
+            <TextField source="totalShipping" />
+            <TextField source="totalTax" />
+            <TextField source="shippingTax" />
+            <TextField source="note" />
+            <TextField source="customer.id" />
+            <TextField source="customer.email" />
+            <TextField source="customer.acceptMarketing" />
+            <TextField source="totalDiscount" />
+            <TextField source="billingAddress.zip" />
+            <TextField source="shippingAddress.zip" />
+            <ArrayField source="lineItems">
+                <Datagrid bulkActionButtons={false}>
+                    <TextField source="title" />
+                    <TextField source="price" />
+                    <NumberField source="quantity" />
+                </Datagrid>
+            </ArrayField>
+            <DateField source="createdAt" />
+            <DateField source="updatedAt" />
+        </Datagrid>
+    </List>
+);
 
 const OrderCreate = (props) => (
     <Create {...props}>
         <SimpleForm>
-            <TextInput source="title" />
-            <TextInput source="teaser" options={{ multiLine: true }} />
-            <DateInput label="Publication date" source="published_at" defaultValue={new Date()} />
         </SimpleForm>
     </Create>
 );
@@ -19,17 +87,6 @@ const OrderCreate = (props) => (
 const OrderEdit = (props) => (
     <Edit {...props}>
         <SimpleForm>
-            <TextInput disabled label="Id" source="id" />
-            <TextInput source="title" validate={required()} />
-            <TextInput multiline source="teaser" validate={required()} />
-            <DateInput label="Publication date" source="published_at" />
-            <ReferenceManyField label="Comments" reference="comments" target="post_id">
-                <Datagrid>
-                    <TextField source="body" />
-                    <DateField source="created_at" />
-                    <EditButton />
-                </Datagrid>
-            </ReferenceManyField>
         </SimpleForm>
     </Edit>
 );
