1a23306b6eee1c8b679d77c027c71b4eee785870
yairyairyair <24393372+yairyairyair@users.noreply.github.com>
2022-07-23 17:52:45 +0300

    add discord api send message implementation

--- packages/server/src/discord-api.ts
+++ packages/server/src/discord-api.ts
@@ -1,8 +1,24 @@
-const { DISCORD_API_TOKEN } = process.env;
+import fetch from 'node-fetch'
 
-// TODO: implement this
-const sendDiscordMessage = (userToSend: string, message: string) => {
+const HEADERS = {
+    'Content-Type': 'application/json'
+}
 
+const sendDiscordMessage = async (webhookToSendTo: string, message: string) => {
+    const discordPayload = { content: message };
+
+    const response = await fetch(`${webhookToSendTo}?wait=true`, {
+        headers: HEADERS,
+        method: 'POST',
+        body: JSON.stringify(discordPayload)
+    });
+
+    if (!response.ok) {
+        const responseText = await response.text();
+        throw new Error(responseText);
+    }
+    const responseJson = await response.json();
+    return responseJson;
 }
 
 export {
